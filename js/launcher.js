65ed83e037aab55e332181d0ac6cfcdf61f63e25
Zakee Shaikh <78408518+zakeeshaikh@users.noreply.github.com>
2021-06-15 13:52:16 +0530

    Add files via upload

---
+++ js/launcher.js
@@ -0,0 +1,42 @@
+class Launcher
+{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+
+        this.pointB = pointB
+        this.bodyA = bodyA
+
+        this.launcher = Constraint.create(options);
+        World.add(world, this.launcher);
+    }
+    attach(body){
+
+        this.launcher.bodyA = body;
+    }
+
+    fly(){
+
+
+     this.launcher.bodyA = null;
+
+    }
+
+    display(){
+
+        if(this.launcher.bodyA){
+
+         
+                                                                                    
+        
+        var pointA = this.launcher.bodyA.position;
+        var pointB = this.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);}
+    }
+    
+}
